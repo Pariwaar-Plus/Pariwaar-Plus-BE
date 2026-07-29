05a112a00f32cea9b1d95cd33145e689ff82f1a9
rohitjoey <roht.praz@gmail.com>
2026-07-29 17:26:58 +1000

    prisma neon integration

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -8,35 +8,38 @@
 
 // export default prisma ;
 
+import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+dotenv.config();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma ;
 
 // import { PrismaClient } from '@prisma/client';
 // import { PrismaPg } from '@prisma/adapter-pg';
+// import { Pool } from 'pg';
+// import dotenv from "dotenv";
 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL!,
-// });
+// // 1. Load the variables IMMEDIATELY
+// dotenv.config();
 
+// const connectionString = process.env.DATABASE_URL;
+
+// // 2. Add a check to catch the error early with a better message
+// if (!connectionString) {
+//   throw new Error("DATABASE_URL is not defined in the environment variables.");
+// }
+
+// const pool = new Pool({ connectionString });
+// const adapter = new PrismaPg(pool);
 // const prisma = new PrismaClient({ adapter });
 
-// export default prisma ;
+// export default prisma;
 
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import dotenv from "dotenv";
 
-// 1. Load the variables IMMEDIATELY
-dotenv.config();
-
-const connectionString = process.env.DATABASE_URL;
-
-// 2. Add a check to catch the error early with a better message
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in the environment variables.");
-}
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
-
-export default prisma;
